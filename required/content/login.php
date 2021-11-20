d5fe0ddb8e7ef8ec2b5893a03613a62b981e72bb
steven122750 <cardona21507@gmail.com>
2021-11-20 16:43:57 -0500

    subiendo proyecto steven

--- required/content/login.php
+++ required/content/login.php
@@ -1,5 +1,6 @@
 <div class="containerLogin">
 <form class="formulario" method="POST" action="codigo.php">
+
     <h1>Iniciar sesion</h1>
     <div class="contenedor">
         <div class="input-contenedor">
@@ -13,5 +14,89 @@
         <input type="submit" value="Iniciar sesion" class="button">
         <p>No tienes una cuenta? <a class="link" href="Registro.html">Registrate</a></p>
     </div>
+    <div class="container">
+  
+  <ul class="slider">
+    <li id="slide1">
+      <img src="media/img1.jpg"/>
+    </li>
+    <li id="slide2">
+      <img src="media/img2.jpg"/>
+    </li>
+    <li id="slide3">
+      <img src="media/img3.jpg"/>
+    </li>
+    <li id="slide4">
+    <img src="media/img4.jpg"/>
+    </li>
+    <li id="slide5">
+    <img src="media/img5.jpg"/>
+    </li>
+    <li id="slide6">
+    <img src="media/img6.jpg"/>
+    </li>
+    <li id="slide7">
+    <img src="media/img7.jpg"/>
+    </li>
+    <li id="slide8">
+    <img src="media/img8.jpg"/>
+    </li>
+    <li id="slide9">
+    <img src="media/img9.jpg"/>
+    </li>
+    <li id="slide10">
+    <img src="media/img10.jpg"/>
+    </li>
+    <li id="slide11">
+    <img src="media/img11.jpg"/>
+    </li>
+    <li id="slide12">
+    <img src="media/img12.jpg"/>
+    </li>
+  </ul>
+  
+  <ul class="menu">
+    <li>
+    <a href="#slide1">3</a>
+    </li>
+    <li>
+      <a href="#slide2">2</a>
+    </li>
+     <li>
+      <a href="#slide3">3</a>
+    </li>
+    <li>
+    <a href="#slide4">4</a>
+    </li>
+    <li>
+    <a href="#slide5">5</a>
+    </li>
+    <li>
+    <a href="#slide6">6</a>
+    </li>
+    <li>
+    <a href="#slide7">7</a>
+    </li>
+    <li>
+    <a href="#slide8">8</a>
+    </li>
+    <li>
+    <a href="#slide9">9</a>
+    </li>
+    <li>
+    <a href="#slide10">10</a>
+    </li>
+    <li>
+    <a href="#slide11">11</a>
+    </li>
+    <li>
+    <a href="#slide12">12</a>
+    </li>
+  </ul>
+  
+</div>
+    
+
+</div>
 </form>
 </div>
--- required/menu/main-menu.php
+++ required/menu/main-menu.php
@@ -4,7 +4,6 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
                 <div class="collapse navbar-collapse" id="navbarsExample01">
                     <ul class="navbar-nav me-auto mb-2">
                         <li class="nav-item">
