4cbd1d2042a80bd8866874251e40ef249668f383
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-21 17:24:44 -0500

    subiendo footer

--- required/footer.php
+++ required/footer.php
@@ -1,24 +1,54 @@
 <div class="containerFooter">
-    <div class="container">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                    <svg class="bi" width="30" height="24">
-                        <use xlink:href="#bootstrap" />
-                    </svg>
-                </a>
-                <span class="text-muted">&copy; 2021 Company, Inc</span>
+    <footer class="py-5">
+        <div class="row">
+            <div class="d-flex justify-content-between">
+                <h2 id="titulo">Store Dark</h2>
+                <ul class="list-unstyled d-flex">
+                    <h3>
+                        <li class="ms-3"><i class="fab fa-twitch"></i></li>
+                    </h3>
+                    <h3>
+                        <li class="ms-3"><i class="fab fa-youtube"></i></li>
+                    </h3>
+                    <h3>
+                        <li class="ms-3"><i class="fab fa-twitter"></i></li>
+                    </h3>
+                    <h3>
+                        <li class="ms-3"><i class="fab fa-instagram"></i></li>
+                    </h3>
+                    <h3>
+                        <li class="ms-3"><i class="fab fa-facebook"></i></li>
+                    </h3>
+                </ul>
+            </div>
+            <div class="py-4 my-4 border-top"></div>
+
+            <div class="col-6 left">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Inicio</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Quienes Somos</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Producto</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Asesor</a></li>
+                </ul>
             </div>
 
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3"><a class="text-muted" href="#"><i class="fab fa-twitter"></i></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><i class="fab fa-instagram"></i></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"><i class="fab fa-facebook"></i></a></li>
-            </ul>
-        </footer>
-    </div>
-</div>
 
+            <div class="col-6 right">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Comunidad</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Centro de Ayuda</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Puntos de venta</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Contacto</a></li>
+                </ul>
+            </div>
+
+            <div class="d-flex justify-content-between py-4 my-4 border-top">
+                <p>&copy; 2021 Company, Inc. All rights reserved.</p>
+                <ul class="list-unstyled d-flex">
+                </ul>
+            </div>
+    </footer>
+</div>
 <script src="bootstrap/js/bootstrap.js"></script>
 <script src="bootstrap/js/popper.min.js"></script>
 </body>
--- required/header.php
+++ required/header.php
@@ -5,12 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $ubicacion;?></title>
+    <title><?php echo $ubicacion; ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Lobster&family=Shadows+Into+Light&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/2a261ef767.js" crossorigin="anonymous"></script>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="new-css/login.css" rel="stylesheet">
     <script>
-        const BASE_ROOT_URL ="<?php echo $BASE_ROOT_URL?>";
+        const BASE_ROOT_URL = "<?php echo $BASE_ROOT_URL ?>";
     </script>
     <script src="js/passwordSame.js"></script>
 </head>
