c20ef972a8dc86aac049cebfeb77fe9bbb2a5c5b
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-22 11:29:24 -0500

    subiendo parte de ventas principal

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ require("data/funciones.php");
 require("required/header.php");
 require("required/menu/main-menu.php");
 require("required/content/login.php");
+require("required/content/ventas.php");
 require("required/footer.php");
