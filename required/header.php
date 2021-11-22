fcc70e066f4d2d31f4367844743166e8c23bb5c7
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-22 17:35:01 -0500

    subiendo vainas

--- required/header.php
+++ required/header.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Lobster&family=Shadows+Into+Light&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Lobster&family=Shadows+Into+Light&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/2a261ef767.js" crossorigin="anonymous"></script>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <script>
