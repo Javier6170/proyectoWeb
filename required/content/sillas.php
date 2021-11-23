38adf248241e29dc478228c08a2075117a1fefed
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-23 11:45:48 -0500

    subiendo mas vainas

---
+++ required/content/sillas.php
@@ -0,0 +1,16 @@
+<section>
+<div class="img-portada">
+    <div class="img">
+        <img src="https://redragon.es/content/uploads/2021/05/CAPRICORNUS-C502-Hero-Desktop-Hero-1920x880-1.jpg" alt="producto 1">
+    </div>
+</div>
+    <div class="card estilo-a">
+        <a href="#">
+            <div class="img-container">
+                <img src="https://redragon.es/content/uploads/2021/05/C502-BR.png" alt="producto 1">
+            </div>
+                <p>CAPRICORNUS C502</p>
+                <p>$1.200.000 COP</p>
+        </a>
+    </div>
+</section>
